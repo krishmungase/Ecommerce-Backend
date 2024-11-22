ed395d1b2bca5d5a73ca15180cfc8dca69d0c5b6
krishmungase <mungasekrishna8@gmail.com>
2024-11-22 20:34:56 +0530

    bugs..

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5515;
 
 app.use(
   cors({
-    origin: "http://localhost:5173" || "https://ecommerce-frontend-ebon-one.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://ecommerce-frontend-ebon-one.vercel.app"
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
